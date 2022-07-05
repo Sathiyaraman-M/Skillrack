d1d426a3495a1370b68876b3038c4c1e42707b2e
Sathiyaraman M <45333880+Sathiyaraman-M@users.noreply.github.com>
2022-07-05 12:09:30 +0530

    Add SlidingWindowMaximumConsonants.py

---
+++ misc/SlidingWindowMaximumConsonants.py
@@ -0,0 +1,8 @@
+K = int(input())
+S = input().strip()
+for i in range(len(S) - K + 1):
+    count = 0
+    for j in S[i: i + K]:
+        if j not in "aeiouAEIOU":
+            count += 1
+    print(count, end = " ")
