da475bf92677542ca97e1d9f6795f063022a790e
Sathiyaraman M <45333880+Sathiyaraman-M@users.noreply.github.com>
2022-07-11 10:44:35 +0530

    Add RemoveAdjacentEqualValues.cs

---
+++ misc/RemoveAdjacentEqualValues.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        var list = Console.ReadLine().Trim().Split(' ').Select(int.Parse).ToList();
+        var stack = new Stack<int>();
+        for(int i = N - 1; i >= 0; i--)
+        {
+            if(stack.Count == 0)
+            {
+                stack.Push(list[i]);
+            }
+            else
+            {
+                if(stack.Peek() == list[i]) stack.Pop();
+                else stack.Push();
+            }            
+        }
+        if(stack.Count == 0)
+        {
+            Console.Write(-1);
+            return;
+        }
+        while(stack.Count > 0)
+        {
+            Console.Write(stack.Pop());
+        }        
+    }
+}
