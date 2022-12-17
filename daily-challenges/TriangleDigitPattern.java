ca2f5d82dad778db4ef77edbeb2905e9ca9232fb
Sathiyaraman M <45333880+Sathiyaraman-M@users.noreply.github.com>
2022-12-17 22:31:56 +0530

    Add TriangleDigitPattern.java

---
+++ daily-challenges/TriangleDigitPattern.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Hello {
+    public static void main(String[] args) {
+        var input = new Scanner(System.in).nextLine().trim();
+        var len = input.length();
+        for(int i = 0; i < len; i++) {
+            var digit = Integer.parseInt(String.valueOf(input.charAt(i)));
+            for(int j = 0; j <= i; j++) {
+                System.out.print(digit);
+            }
+            System.out.println();
+        }
+    }
+}
