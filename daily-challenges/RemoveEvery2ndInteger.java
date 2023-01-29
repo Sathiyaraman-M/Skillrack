689096de7e93007b801a7a11db045d165349acd3
Sathiyaraman M <45333880+Sathiyaraman-M@users.noreply.github.com>
2023-01-29 22:33:42 +0530

    Add RemoveEvery2ndInteger.java

---
+++ daily-challenges/RemoveEvery2ndInteger.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Hello {
+    public static void main(String[] args) {
+        var sc = new Scanner(System.in);
+        var N = sc.nextInt();
+        for(int i = 0; i < N; i++) {
+            var val = sc.nextInt();
+            if(i % 2 == 0) {
+                System.out.printf("%d ", val);
+            }
+        }
+    }
+}
